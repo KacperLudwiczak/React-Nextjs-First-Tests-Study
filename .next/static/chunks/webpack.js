427ee88dfe1b01a17ef5cfa4906aaea55e77d440
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-27 17:10:26 +0200

    links to pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "71a37ab02e421459"; }
+/******/ 		__webpack_require__.h = function() { return "4d0c0416bfd52e25"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/first-article">First Article</Link>
+        </li>
+        <li>
+          <Link href="/news/second-article"> Second Article</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
