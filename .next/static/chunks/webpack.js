e73492ecffaff46125a28824c1f4b5015013059f
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-27 17:02:33 +0200

    useRouter to have newsId

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "42e962b0507f2391"; }
+/******/ 		__webpack_require__.h = function() { return "71a37ab02e421459"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
